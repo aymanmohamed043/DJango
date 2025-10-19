d1e4ad80a1a43492d820e9cea2790e8cac376771
ayman <ayman.mohamed141163@el-eng.menofia.edu.eg>
2025-10-20 00:32:42 +0300

    connect to admin panel and start to custamize it start with product and edit display list make unit price editable

--- djangobasics/store/admin.py
+++ djangobasics/store/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
+    list_editable = ['unit_price']
 # store app models
 admin.site.register(Customer)
 admin.site.register(Address)
-admin.site.register(Product)
+# admin.site.register(Product)
 admin.site.register(Collection)
 admin.site.register(Cart)
 admin.site.register(CartItem)
