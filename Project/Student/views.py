83e2686531b22a23ae0c7d7cfd7138113d9dba78
ayman <ayman.mohamed141163@el-eng.menofia.edu.eg>
2025-10-20 22:30:58 +0300

    setup project sittings, urls, template and static files and folders

--- Project/Student/views.py
+++ Project/Student/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home (request): 
+    return render(request, 'base.html')
+
+def students_list(request):
+    return render(request, 'students_list.html')
