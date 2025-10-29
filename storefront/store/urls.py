d38eb991fb84d9f8f470a8738dfe14163e3a3930
ayman <ayman.mohamed141163@el-eng.menofia.edu.eg>
2025-10-29 21:26:22 +0300

    update view file by class base method and use APIView

--- storefront/store/urls.py
+++ storefront/store/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('products/', product_list, name='product-list'),
-    path('products/<int:id>', product_detail, name='product-detail'),
+    path('products/', ProductAPIView.as_view(), name='product-list'),
+    path('products/<int:pk>', ProductAPIViewDetails.as_view(), name='product-detail'),
 ]
--- storefront/store/views.py
+++ storefront/store/views.py
@@ -1,30 +1,48 @@
 from rest_framework.response import Response 
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView    
 from rest_framework import status
 from .models import *
 from .serializer import *
 # Create your views here.
 
 
-@api_view(['GET', 'POST'])
-def product_list(request):
-    if request.method == 'GET':
-
-        queryset = Product.objects.prefetch_related('collection').all()
-        serializer = ProductSerializer(queryset, many=True)
+class ProductAPIView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
-
-    elif request.method == 'POST':
+    
+    def post(self, request):
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return RecursionError(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'POST'])
-def product_detail(request, id):
-    product = Product.objects.get(pk=id)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+
+class ProductAPIViewDetails(APIView):
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except:
+            return Response({"error": "Post not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+    def get(self, request, pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+
+    def delete(self, request, pk):
+        product = self.get_object(pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
